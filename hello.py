520880e71115ee689f8cc53fded24f04ee292ec5
HAIRUINI <hairuini@HAIRUINIdeMacBook-Pro.local>
2025-06-06 15:09:24 -0400

    更新了 hello.py 文件

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
 print("Hi GitHub, this is Harriet's first commit!")
+
+"""
+克隆代码
+cd ~/Desktop
+git clone https://github.com/HarietNi/bart-bcf-notebooks.git
+cd bart-bcf-notebooks
+code .
+
+push代码到GitHub仓库
+git add hello.py
+git commit -m "feat: add hello.py test file"
+git push
+"""
